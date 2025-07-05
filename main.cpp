ee3d904a0b4f9c6871f5558fcb7c8bdb490e7a6f
JackBlitZ <luke@heybuildthis.com>
2025-07-04 17:12:54 -0700

    Inital Main function
    Application Main

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <library.h>
+
+int main() {
+    std::cout << "Vega Win32 Application" << std::endl;
+    hello(); // Call the engine library function
+    return 0;
+}
