b2efd0efa4891126785a73f49241e9294d97403e
jackblitz <jackblitz@example.com>
2025-07-07 19:59:07 -0700

    Basic Application Creation

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
-
+#include "VegaApplication.h"
+#include "polaris/source/runtime/core/Logger.h" // Include the Logger header
 
 int main() {
-    std::cout << "Vega Win32 Application" << std::endl;
+    // Initialize the logger at the very beginning
+    polaris::Logger::getInstance().initialize("vega42.log"); // You can specify a log file name
+
+    LOG_INFO("Vega Win32 Application starting..."); // Example log message
+
+    vega::VegaApplication *application = new vega::VegaApplication();
+    application->initialize();
+    application->run();
+
+    delete application; // Don't forget to delete dynamically allocated objects
+
+    LOG_INFO("Vega Win32 Application shutting down."); // Example log message
+
+    // Shutdown the logger at the very end
+    polaris::Logger::getInstance().shutdown();
+
     return 0;
 }
